c1fabc87e1825f798f8622a6a9c25c0ce7304a1b
Clodomir <clovianna@gmail.com>
2023-01-21 15:10:45 -0300

    debbug trial 7

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const signin = require('./controllers/signin');
 
 const app = express();
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 //app.use(express.json());
 
