576aa226827f1d71de0a2b4a2086361e84f5267f
Clodomir <clovianna@gmail.com>
2023-01-22 10:52:42 -0300

    database trials 4

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
 
 //app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
 
-app.post('/register', (req, res) => {res.json(res.body);})
+app.post('/register', (req, res) => {res.json(req.body);})
 
 app.get('/profile/:id', (req, res) => {
 
