d01325e9b11bceb02a577b3ee2949ef59c177246
Clodomir <clovianna@gmail.com>
2023-01-23 20:00:30 -0300

    db trials 12

--- server.js
+++ server.js
@@ -56,7 +56,39 @@ app.post('/register', (req, res) => {
     console.log('req', req.body);
     console.log('hash', hash);
     
-    db.select('*').from('users').then(data => {res.json(data)});
+    //db.select('*').from('users').then(data => {res.json(data)});
+
+    db.transaction(trx => {
+
+        trx.insert({
+
+            hash: hash,
+            email: email
+
+        })
+        .into('login')
+        .returning('email')
+        .then(loginEmail => {
+
+            return trx('users').returning('*').insert({
+
+                email: loginEmail[0].email,
+                name: name,
+                color: favoriteColor,
+                joined: new Date
+        
+            }).then(user => {
+        
+                res.json(user[0]);
+        
+            })
+            .then(trx.commit)
+            .catch(trx.rollback);
+
+
+        }).catch(err => res.status(400).json('Unable to register.'))
+        
+    })
 
 })
 
